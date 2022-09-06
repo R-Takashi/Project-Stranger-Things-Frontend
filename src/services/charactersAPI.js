31cad7ed1ca7a8d5e9f2579b90d94169c3da0736
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-09-06 18:22:06 -0300

    deploy frontend feature

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
-  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
+  constructor({ url = 'https://r-takashi-ft.herokuapp.com/', timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
